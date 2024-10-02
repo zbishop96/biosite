b2c2e4bf970d6bc997bc1695b0a1c90c0d70cb03
zbishop96 <zbishop96@gmail.com>
2024-10-02 04:25:21 -0700

    More verbose profile card

--- app/gaming/SteamProfileCard.tsx
+++ app/gaming/SteamProfileCard.tsx
@@ -3,6 +3,15 @@ import { Card, CardHeader, CardBody, Image } from "@nextui-org/react"
 export default async function SteamProfileCard() {
 
   const profileData = await getProfileData()
+  let profileStatus = 'Currently offline'
+  if (profileData.personastate === 1 && profileData.gameid === undefined) {
+    profileStatus = 'Currently online'
+  } else if (profileData.personastate === 1 && profileData.gameid) {
+    profileStatus = `Currently playing ${profileData.gameextrainfo}`
+  } else if (profileData.personastate === 3) {
+    profileStatus = 'Currently away'
+  }
+
   return <Card className="shadow">
     <CardBody className="overflow-visible flex-row gap-3 items-end">
         <Image
@@ -12,8 +21,8 @@ export default async function SteamProfileCard() {
           isBlurred
         />
         <div className="flex-col">
-            <h1 className="text-large font-bold line-clamp-1">Alfie Solomons</h1>
-            <h1 className="text-base font-semibold line-clamp-1">{profileData.gameid === undefined ? `Not currently playing` : `Currently playing ${profileData.gameextrainfo}`}</h1>
+            <h1 className="text-large font-bold line-clamp-1">{profileData.personaname}</h1>
+            <h1 className="text-base font-semibold line-clamp-1">{profileStatus}</h1>
         </div>
       </CardBody>
     </Card>
@@ -21,8 +30,7 @@ export default async function SteamProfileCard() {
 }
 
 async function getProfileData() {
-    const response = await fetch(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${process.env.STEAM_API_KEY}&steamids=${process.env.STEAM_PROFILE_ID}`, { next: { revalidate: 20 }})
-  
+    const response = await fetch(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${process.env.STEAM_API_KEY}&steamids=${process.env.STEAM_PROFILE_ID}`, { next: { revalidate: 10 }})
     const data = await response.json()
     return data.response.players[0]
   }
